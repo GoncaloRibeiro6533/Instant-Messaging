a6531f5a17321326d1f5995ccbc0cda2db7b9832
GoncaloRibeiro6533 <goncaloribeiro6533@gmail.com>
2024-10-14 10:54:38 +0100

    added authentication utils

--- code/jvm/service/src/main/kotlin/Utils.kt
+++ code/jvm/service/src/main/kotlin/Utils.kt
@@ -1,6 +1,17 @@
 
-
-// TODO("To delete")
-private fun utils() {
-    println("test")
+sealed class AuthenticationError {
+    data object Unauthorized : AuthenticationError()
+    data object SessionExpired: AuthenticationError()
+    data object UserNotFound : AuthenticationError()
 }
+
+fun authenticate(trxManager: TransactionManager, token: String): Either<AuthenticationError, User> =
+    trxManager.run {
+        val session = sessionRepo.findByToken(token) ?: return@run failure(AuthenticationError.Unauthorized)
+        if (session.expiration < Instant.now()) {
+            sessionRepo.delete(session)
+            return@run failure(AuthenticationError.SessionExpired)
+        }
+        val user = userRepo.findById(session.userId) ?: return@run failure(AuthenticationError.UserNotFound)
+        return@run success(user)
+    }
