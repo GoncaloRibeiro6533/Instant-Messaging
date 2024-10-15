fde690a7ee98c3eb73b8debf3741db6d6bc570ad
GoncaloRibeiro6533 <goncaloribeiro6533@gmail.com>
2024-10-15 19:23:08 +0100

    fixed tests

--- code/jvm/repository-jdbi/src/main/kotlin/JdbiInvitationRepository.kt
+++ code/jvm/repository-jdbi/src/main/kotlin/JdbiInvitationRepository.kt
@@ -1,6 +1,51 @@
 import org.jdbi.v3.core.Handle
 
 class JdbiInvitationRepository(
-    private val handle: Handle
-) :InvitationRepository{
+    private val handle: Handle,
+) : InvitationRepository {
+    override fun createRegisterInvitation(
+        sender: User,
+        email: String,
+        channel: Channel,
+        role: Role,
+    ): RegisterInvitation {
+        TODO("Not yet implemented")
+    }
+
+    override fun createChannelInvitation(
+        sender: User,
+        receiver: User,
+        channel: Channel,
+        role: Role,
+    ): ChannelInvitation {
+        TODO("Not yet implemented")
+    }
+
+    override fun findRegisterInvitationById(invitationId: Int): Invitation? {
+        TODO("Not yet implemented")
+    }
+
+    override fun findChannelInvitationById(invitationId: Int): Invitation? {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateRegisterInvitation(invitation: Invitation): Invitation {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateChannelInvitation(invitation: Invitation): Invitation {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteRegisterInvitationById(invitationId: Int): Invitation {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteChannelInvitationById(invitationId: Int): Invitation {
+        TODO("Not yet implemented")
+    }
+
+    override fun getInvitationsOfUser(user: User): List<Invitation> {
+        TODO("Not yet implemented")
+    }
 }
--- code/jvm/repository-jdbi/src/main/kotlin/JdbiMessageRepository.kt
+++ code/jvm/repository-jdbi/src/main/kotlin/JdbiMessageRepository.kt
@@ -1,6 +1,25 @@
 import org.jdbi.v3.core.Handle
 
-class JdbiMessageRepository (
-    private val handle: Handle
-) :MessageRepository{
+class JdbiMessageRepository(
+    private val handle: Handle,
+) : MessageRepository {
+    override fun findById(id: Int): Message? {
+        TODO("Not yet implemented")
+    }
+
+    override fun sendMessage(
+        sender: User,
+        channel: Channel,
+        text: String,
+    ): Message {
+        TODO("Not yet implemented")
+    }
+
+    override fun getMsgHistory(
+        channel: Channel,
+        limit: Int,
+        skip: Int,
+    ): List<Message> {
+        TODO("Not yet implemented")
+    }
 }
