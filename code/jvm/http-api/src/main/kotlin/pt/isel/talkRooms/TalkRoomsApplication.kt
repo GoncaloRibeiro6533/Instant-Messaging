48d314d8f78fea4b8ddfa8a6308365c4563bd88a
Tiago <A48252@alunos.isel.pt>
2024-10-16 19:57:27 +0100

    fix

--- code/jvm/http-api/src/main/kotlin/pt/isel/talkRooms/TalkRoomsApplication.kt
+++ code/jvm/http-api/src/main/kotlin/pt/isel/talkRooms/TalkRoomsApplication.kt
@@ -2,22 +2,23 @@ package pt.isel.talkRooms
 
 import TransactionManagerJdbi
 import configureWithAppRequirements
+import kotlinx.datetime.Clock
+import org.jdbi.v3.core.Jdbi
+import org.postgresql.ds.PGSimpleDataSource
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
-import org.jdbi.v3.core.Jdbi
-import org.postgresql.ds.PGSimpleDataSource
 import org.springframework.context.annotation.Profile
-import kotlinx.datetime.Clock
 
 @SpringBootApplication
 class TalkRoomsApplication {
     @Bean
     fun jdbi() =
         Jdbi
-            .create(PGSimpleDataSource().apply {
-                setURL(Environment.getDbUrl())
-            }
+            .create(
+                PGSimpleDataSource().apply {
+                    setURL(Environment.getDbUrl())
+                },
             ).configureWithAppRequirements()
 
     @Bean
--- code/jvm/repository-jdbi/src/main/kotlin/JdbiChannelRepository.kt
+++ code/jvm/repository-jdbi/src/main/kotlin/JdbiChannelRepository.kt
@@ -88,5 +88,4 @@ class JdbiChannelRepository(
             .execute()
             .let { channel }
     }
-
 }
