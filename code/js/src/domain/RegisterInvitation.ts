5798240ce6d5df7e5640578786020a1d12b3e9cb
46042 - Rosario Machado <A46042@alunos.isel.pt>
2024-12-02 09:55:41 +0000

    RegisterInvitation domain corrected

--- code/js/src/domain/RegisterInvitation.ts
+++ code/js/src/domain/RegisterInvitation.ts
@@ -1,11 +1,13 @@
 import {Role} from "./Role";
 import {User} from "./User";
+import {Channel} from "./Channel";
 
 
 export type RegisterInvitation = {
     id: number,
     sender: User,
-    receiver: User,
+    email: string,
+    channel: Channel,
     role: Role,
     isUsed: boolean,
     timestamp: Date,
